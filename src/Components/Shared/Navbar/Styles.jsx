1c181ac2118012525e9314679e580954dcb24e9c
farbs03 <cgeraldfarber@icloud.com>
2021-03-24 15:47:02 -0400

    better shadows on sidenav

--- src/Components/Shared/Navbar/Styles.jsx
+++ src/Components/Shared/Navbar/Styles.jsx
@@ -11,7 +11,6 @@ const useStyles = makeStyles((theme) => ({
     drawer: {
       width: drawerWidth,
       flexShrink: 0,
-      boxShadow: "2px 0px 12px rgba(0,0,0,0.1)"
     },
     drawerPaper: {
       width: drawerWidth,
