4950f31bbdc1f70a114f604b20d5d495d919a41c
farbs03 <cgeraldfarber@icloud.com>
2021-03-24 22:03:59 -0400

    fixed merge errors

--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -6,42 +6,8 @@ import useStyles from "./Styles"
 import Form from "./Form/Form"
 
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-<<<<<<< HEAD
 import {Grid, CircularProgress, Typography, Grow } from "@material-ui/core"
-import { useSelector } from "react-redux"
-import Form from "../Form/Form"
 
-=======
-import {Grid, Grow } from "@material-ui/core"
-
-/* const periodV = {
-    hidden:{
-       opacity: 0,
-       x: '200%'
-    },
-    visible:{
-        opacity: 1,
-        x: 0,
-        transition: {
-            type: 'spring',
-            stiffness: 50
-        }
-    }
-}
-
-const bigContainerVariants = {
-    hidden:{
-        opacity: 0
-    },
-    visible:{
-        opacity: 1,
-        transition: {
-            staggerChildren: "0.10"
-        }
-    }
-}
- */
->>>>>>> d6c2bb195560b60d8c8e5554619364f9d8120d2c
 const Tasks = () => {
     const classes = useStyles();
     const mobile = useMediaQuery('(max-width:750px)');
