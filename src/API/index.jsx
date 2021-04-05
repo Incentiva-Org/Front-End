8a43ecd9560478b74ccdaa6320fb021964c7918a
farbs03 <cgeraldfarber@icloud.com>
2021-04-05 19:18:39 -0400

    minor change

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/tasks';
+const url = 'https://incentiva-server/tasks';
 
 export const fetchTasks = () => axios.get(url);
 export const createTask = (newTask) => axios.post(url, newTask);
 export const editTask = (id, newTask) => axios.patch(`${url}/${id}`, newTask);
 export const deleteTask = (id) => axios.delete(`${url}/${id}`);
 
-const authUrl = 'http://localhost:5000/users/';
+const authUrl = 'https://incentiva-server/users/';
 
 export const createUser = (newUser) => axios.post(`${authUrl}/signup`, newUser)
