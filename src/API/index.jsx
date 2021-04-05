a3c17f14e81fd22874739bb1dec5f7dd7a441c2c
farbs03 <cgeraldfarber@icloud.com>
2021-04-05 19:30:38 -0400

    minor changes

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const url = 'https://incentiva-server/tasks';
+const url = 'https://incentiva-server.herokuapp.com/tasks';
 
 export const fetchTasks = () => axios.get(url);
 export const createTask = (newTask) => axios.post(url, newTask);
 export const editTask = (id, newTask) => axios.patch(`${url}/${id}`, newTask);
 export const deleteTask = (id) => axios.delete(`${url}/${id}`);
 
-const authUrl = 'https://incentiva-server/users/';
+const authUrl = 'https://incentiva-server.herokuapp.com/users/';
 
 export const createUser = (newUser) => axios.post(`${authUrl}/signup`, newUser)
